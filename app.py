315cc6d6c3a9151ac98333e4496e789dee1c5a98
taufiqalhasib <75545404+taufiqalhasib@users.noreply.github.com>
2023-06-02 22:48:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ if selected == "First Innings Score":
             (OneHotEncoder(sparse=False, drop='first'), [
                 'batting_team', 'bowling_team', 'venue', 'gender'
             ]),
-            (MinMaxScaler(), [
+            (StandardScaler(), [
                 'current_score', 'balls_bowled', 'balls_left', 'wickets_left', 'crr'
             ])
         )
